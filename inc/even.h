1f04f81eaa1b1990521f0b06f34e9b10136b2ddf
stepin104645 <69105722+stepin104645@users.noreply.github.com>
2020-09-17 13:00:33 +0530

    Update even.h

--- inc/even.h
+++ inc/even.h
@@ -1,18 +1,8 @@
+  
+#ifndef __SUM_H__
+#define __SUM_H__
 
-///@file even.h
+int sum(int number1, int number2);
 
 
-
-#ifndef EVEN_H_INCLUDED
-#define EVEN_H_INCLUDED
-/**
-* To check integer number to be a prime or not
-* @param[in] number to check prime or not
-* @return true if prime ,false if not prime number
-* @note Returns false for negative values
-*/
-
-int even(int number);
-
-#endif /* #ifndef __PRIME_H__ */
-
+#endif
